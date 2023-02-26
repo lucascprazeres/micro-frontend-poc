1bcd4ff509f49d9cae8a4e2c956adbec1377761b
Lucas Prazeres <lucascprazeres@outlook.com>
2023-02-26 19:55:48 -0300

    chore: setup hero

--- container/src/pages/index.tsx
+++ container/src/pages/index.tsx
@@ -1,19 +1,14 @@
 import Head from 'next/head'
 import { DefaultLayout } from '../layouts/DefaultLayout'
 
-import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
-
 import {
-  HeroSection,
   HomeContainer,
   HomeContent,
-  Title,
-  Description,
-  Advantages
 } from '../styles/pages/Home'
 
-import { AdvantageItem } from '../components/AdvantageItem'
-
+// @ts-ignore
+import { HeroSection } from '@lucascprazeres/hero'
+// @ts-ignore
 import { ProductsSection } from '@lucascprazeres/products'
 
 export default function Home() {
@@ -26,37 +21,7 @@ export default function Home() {
       <DefaultLayout>
         <HomeContainer>
           <HomeContent>
-            <HeroSection>
-              <div>
-                <div>
-                  <Title>Encontre o café perfeito para qualquer hora do dia</Title>
-                  <Description>
-                    Com o Coffee Delivery você recebe seu café onde estiver, a
-                    qualquer hora
-                  </Description>
-                </div>
-
-                <Advantages>
-                  <AdvantageItem icon={ShoppingCart} bgColor="#C47F17">
-                    <p>Compra simples e segura</p>
-                  </AdvantageItem>
-
-                  <AdvantageItem icon={Package} bgColor="#574F4D">
-                    <p>Embalagem mantém o café intacto</p>
-                  </AdvantageItem>
-
-                  <AdvantageItem icon={Timer} bgColor="#DBAC2C">
-                    <p>Entrega rápida e rastreada</p>
-                  </AdvantageItem>
-
-                  <AdvantageItem icon={Coffee} bgColor="#8047F8">
-                    <p>O café chega fresquinho até você</p>
-                  </AdvantageItem>
-                </Advantages>
-              </div>
-              <img src="bg-hero.png" alt="" />
-            </HeroSection>
-
+            <HeroSection />
             <ProductsSection />
           </HomeContent>
         </HomeContainer>
